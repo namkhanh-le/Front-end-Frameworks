89723f31dbf629a92e00e9029239e91398cf9aee
Le Nam Khanh <lenamkhanh@MacBook-Pro-cua-Le.local>
2026-09-03 22:54:21 +0200

    EX 1-7

--- exercises/05.js
+++ exercises/05.js
@@ -4,14 +4,13 @@
 const genres = ["Action", "Sci-Fi", "Thriller"];
 
 // 1. Destructure only the first genre.
-
+const [firstGenre1] = genres;
 // 2. Destructure the first and third genre (skip the second — leave the slot empty with a comma).
-
+const [firstGenre2, , thirdGenre2] = genres;
 // 3. Destructure the first genre, and collect the rest into a variable called remainingGenres.
-
+const [firstGenre3, ...remainingGenres] = genres;
 // 4. Swap these two variables without a temp variable:
-let a = "Action";
-let b = "Comedy";
+[a,b] = [b,a];
 // Swap a and b using array destructuring.
 
 console.log(genres);          // ["Action", "Sci-Fi", "Thriller"] — should be unchanged
